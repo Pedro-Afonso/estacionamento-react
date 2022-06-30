e6603f2ff62e60f1e72566f62b2c5bd6c002059a
Pedro-Afonso <pedro.766@hotmail.com>
2022-06-30 18:21:48 -0300

    feat: Adicionado a página Dashboard

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
 import { Route, Routes } from "react-router-dom";
+import { Dashboard } from "../pages";
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<p>teste</p>} />
+      <Route path="/pagina-inicial" element={<Dashboard />} />
     </Routes>
   );
 };
