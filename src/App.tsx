72bcecc98323027b798d0573ca1ddca02dcd079f
Pedro-Afonso <pedro.766@hotmail.com>
2022-06-30 16:13:58 -0300

    feat: Adicionado a biblioteca material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
+
 export const App = () => {
-  return <div>teste</div>;
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
 };
 
 export default App;
