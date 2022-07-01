df7273265aebe8dd27340b54339b31e0053577ef
Pedro-Afonso <pedro.766@hotmail.com>
2022-07-01 19:58:20 -0300

    feat: Configurado o LayoutBaseDaPagina

--- src/shared/layout/LayoutBaseDaPagina.tsx
+++ src/shared/layout/LayoutBaseDaPagina.tsx
@@ -1,9 +1,58 @@
-import { Box } from "@mui/material";
+import {
+  Icon,
+  IconButton,
+  Theme,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import { useDrawerContext } from "../contexts";
 
-export const LayoutBaseDaPagina = () => {
+interface ILayoutBaseDaPaginaProps {
+  titulo: string;
+  children: React.ReactNode;
+  barraDeFerramentas?: React.ReactNode;
+}
+
+export const LayoutBaseDaPagina: React.FC<ILayoutBaseDaPaginaProps> = ({
+  children,
+  titulo: title,
+  barraDeFerramentas,
+}) => {
+  const smDown = useMediaQuery((theme: Theme) => theme.breakpoints.down("sm"));
+  const mdDown = useMediaQuery((theme: Theme) => theme.breakpoints.down("md"));
+  const theme = useTheme();
+
+  const { toggleDrawer } = useDrawerContext();
+
   return (
-    <Box height="100%" width="100%">
-      teste
+    <Box height="100%" display="flex" flexDirection="column" gap={1}>
+      <Box
+        padding={1}
+        height={theme.spacing(smDown ? 6 : mdDown ? 8 : 12)}
+        display="flex"
+        alignItems="center"
+        gap={1}
+      >
+        {smDown && (
+          <IconButton onClick={toggleDrawer}>
+            <Icon>menu</Icon>
+          </IconButton>
+        )}
+        <Typography
+          variant={smDown ? "h5" : mdDown ? "h4" : "h3"}
+          whiteSpace="nowrap"
+          overflow="hidden"
+          textOverflow="ellipsis"
+        >
+          {title}
+        </Typography>
+      </Box>
+      {barraDeFerramentas && <Box>{barraDeFerramentas}</Box>}
+      <Box flex={1} overflow="auto">
+        {children}
+      </Box>
     </Box>
   );
 };
