93c592329f20b35ae4162368dcedcd95dc5e3975
Pedro-Afonso <pedro.766@hotmail.com>
2022-07-01 19:59:21 -0300

    feat: Adicionado o LayoutBaseDaPagina no Dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,9 @@
 import { LayoutBaseDaPagina } from "../shared/layout";
 
 export const Dashboard = () => {
-  return <LayoutBaseDaPagina></LayoutBaseDaPagina>;
+  return (
+    <LayoutBaseDaPagina titulo="Painel" barraDeFerramentas={<p>Barra de Ferramenta</p>}>
+      teste
+    </LayoutBaseDaPagina>
+  );
 };
