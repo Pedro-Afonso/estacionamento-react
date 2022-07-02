2e2b139788213463270858a9e815f65cf20fea8b
Pedro-Afonso <pedro.766@hotmail.com>
2022-07-02 16:09:42 -0300

    feat: Configurações na listagem de veiculos

--- src/pages/ListagemDeVeiculos.tsx
+++ src/pages/ListagemDeVeiculos.tsx
@@ -1,16 +1,68 @@
+import { useEffect, useMemo, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import {
+  Icon,
+  IconButton,
+  LinearProgress,
+  Pagination,
   Paper,
   Table,
   TableBody,
   TableCell,
   TableContainer,
+  TableFooter,
   TableHead,
   TableRow,
 } from "@mui/material";
+
+import { Environment } from "../shared/environment";
+import {
+  VeiculosService,
+  IListagemVeiculo,
+} from "../shared/services/api/veiculos/VeiculosService";
 import { FerramentasDaListagem } from "../shared/components";
 import { LayoutBaseDaPagina } from "../shared/layout";
 
 export const ListagemDeVeiculos = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const navigate = useNavigate();
+
+  const [rows, setRows] = useState<IListagemVeiculo[]>([]);
+  const [totalCount, setTotalCount] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const pagina = useMemo(() => {
+    return Number(searchParams.get("pagina") || 1);
+  }, [searchParams]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    VeiculosService.getAll(pagina).then((res) => {
+      setIsLoading(false);
+
+      if (res instanceof Error) {
+        alert(res.message);
+      } else {
+        setTotalCount(res.totalCount);
+        setRows(res.data);
+      }
+    });
+  }, [pagina]);
+
+  const handleDelete = (id: number) => {
+    if (window.confirm("Realmente deseja apagar?")) {
+      VeiculosService.deleteById(id).then((result) => {
+        if (result instanceof Error) {
+          alert(result.message);
+        } else {
+          setRows((oldRows) => [...oldRows.filter((oldRow) => oldRow.id !== id)]);
+          alert("Registro apagado com sucesso!");
+        }
+      });
+    }
+  };
+
   return (
     <LayoutBaseDaPagina
       titulo="Listagem de Veiculos"
@@ -27,13 +79,44 @@ export const ListagemDeVeiculos = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            <TableRow>
-              <TableCell>Ações</TableCell>
-              <TableCell>João</TableCell>
-              <TableCell>ABC12345</TableCell>
-              <TableCell>10:00 01/07/2022</TableCell>
-            </TableRow>
+            {rows.map((row) => (
+              <TableRow key={row.id}>
+                <TableCell size="small">
+                  <IconButton onClick={() => handleDelete(row.id)}>
+                    <Icon>delete</Icon>
+                  </IconButton>
+                  <IconButton onClick={() => navigate(`/veiculos/detalhe/${row.id}`)}>
+                    <Icon>edit</Icon>
+                  </IconButton>
+                </TableCell>
+                <TableCell>{row.usuario}</TableCell>
+                <TableCell>{row.placa}</TableCell>
+                <TableCell>{row.entrada}</TableCell>
+              </TableRow>
+            ))}
           </TableBody>
+          <TableFooter>
+            {isLoading && (
+              <TableRow>
+                <TableCell colSpan={4}>
+                  <LinearProgress variant="indeterminate" />
+                </TableCell>
+              </TableRow>
+            )}
+            {totalCount > Environment.LIMITE_DE_LINHAS && (
+              <TableRow>
+                <TableCell colSpan={4}>
+                  <Pagination
+                    page={pagina}
+                    count={Math.ceil(totalCount / Environment.LIMITE_DE_LINHAS)}
+                    onChange={(_, newPage) =>
+                      setSearchParams({ pagina: newPage.toString() })
+                    }
+                  />
+                </TableCell>
+              </TableRow>
+            )}
+          </TableFooter>
         </Table>
       </TableContainer>
     </LayoutBaseDaPagina>
