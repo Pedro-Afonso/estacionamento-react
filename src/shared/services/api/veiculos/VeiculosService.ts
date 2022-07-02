c517a465b2e3232c3db88d7c8b9efb5fe45dd864
Pedro-Afonso <pedro.766@hotmail.com>
2022-07-02 11:35:01 -0300

    feat: Configurado o service de veiculos

---
+++ src/shared/services/api/veiculos/VeiculosService.ts
@@ -0,0 +1,104 @@
+import { Api } from "../axios-config";
+
+export interface IListagemVeiculo {
+  id: number;
+  usuario: string;
+  placa: string;
+  entrada: number;
+}
+
+interface IDetalhesVeiculo {
+  id: number;
+  usuario: string;
+  placa: string;
+  entrada: number;
+}
+
+type TVeiculosComTotalCount = {
+  data: IListagemVeiculo[];
+  totalCount: number;
+};
+
+const getAll = async (page = 1): Promise<TVeiculosComTotalCount | Error> => {
+  try {
+    const urlRelativa = `/veiculos?_page=${page}&_limit=${process.env.REACT_APP_LIMITE_DE_LINHAS}`;
+
+    const { data, headers } = await Api.get(urlRelativa);
+
+    if (data) {
+      return {
+        data,
+        totalCount: Number(
+          headers["x-total-count"] || process.env.REACT_APP_LIMITE_DE_LINHAS
+        ),
+      };
+    }
+
+    return new Error("Erro ao listar os registros.");
+  } catch (error) {
+    console.log(error);
+    return new Error(
+      (error as { message: string }).message || "Erro ao listar os registros."
+    );
+  }
+};
+const getById = async (id: number): Promise<IDetalhesVeiculo | Error> => {
+  try {
+    const { data } = await Api.get(`/veiculos/${id}`);
+
+    if (data) {
+      return data;
+    }
+
+    return new Error("Erro ao consultar o registro.");
+  } catch (error) {
+    console.log(error);
+    return new Error(
+      (error as { message: string }).message || "Erro ao consultar o registro."
+    );
+  }
+};
+const create = async (dados: Omit<IDetalhesVeiculo, "id">): Promise<number | Error> => {
+  try {
+    const { data } = await Api.post<IDetalhesVeiculo>("/veiculos", dados);
+
+    if (data) {
+      return data.id;
+    }
+
+    return new Error("Erro ao criar o registro.");
+  } catch (error) {
+    console.log(error);
+    return new Error(
+      (error as { message: string }).message || "Erro ao criar o registro."
+    );
+  }
+};
+const updateById = async (id: number, dados: IDetalhesVeiculo): Promise<void | Error> => {
+  try {
+    await Api.put(`/veiculos/${id}`, dados);
+  } catch (error) {
+    console.log(error);
+    return new Error(
+      (error as { message: string }).message || "Erro ao atualizar o registro."
+    );
+  }
+};
+const deleteById = async (id: number): Promise<void | Error> => {
+  try {
+    await Api.delete(`/veiculos/${id}`);
+  } catch (error) {
+    console.log(error);
+    return new Error(
+      (error as { message: string }).message || "Erro ao apagar o registro."
+    );
+  }
+};
+
+export const VeiculosService = {
+  getAll,
+  getById,
+  create,
+  updateById,
+  deleteById,
+};
