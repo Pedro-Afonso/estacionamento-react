94dc58eb3a57ee8f61de32693de5341b773dc9c4
Pedro-Afonso <pedro.766@hotmail.com>
2022-07-02 10:33:44 -0300

    feat: Adicionado response e error interceptors

--- src/shared/services/api/axios-config/index.ts
+++ src/shared/services/api/axios-config/index.ts
@@ -1,7 +1,13 @@
 import axios from "axios";
+import { errorInterceptor, responseInterceptor } from "./interceptors";
 
 const Api = axios.create({
   baseURL: process.env.REACT_APP_URL_BASE,
 });
+
+Api.interceptors.response.use(
+  (response) => responseInterceptor(response),
+  (error) => errorInterceptor(error)
+);
 
 export { Api };
